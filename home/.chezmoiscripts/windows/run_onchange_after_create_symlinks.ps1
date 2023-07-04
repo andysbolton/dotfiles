bef41e97693b5aea754b0cdf1dec91fb69cadcba
Andy Bolton <andy.bolton@smartwyre.com>
2023-07-03 18:35:57 -0600

    Finish up symlinks for pwsh

--- home/.chezmoiscripts/windows/run_onchange_after_create_symlinks.ps1
+++ home/.chezmoiscripts/windows/run_onchange_after_create_symlinks.ps1
@@ -1,7 +1,21 @@
-Start-Process pwsh -Verb "RunAs" -ArgumentList ("-NoExit", "-Command", "New-Item", "-ItemType", "symboliclink", "-Path", "~/.config/nvim", "-Name", "nvim", "-Value", "~/AppData/Local/nvim")
+function Test-IsSymlink($Path) {
+    $item = Get-Item $Path -ErrorAction SilentlyContinue
+    if ($item -and $item.LinkType -eq "SymbolicLink") {
+        return $true
+    }
+    return $false
+}
+
+$nvimLink = "~/AppData/Local/nvim"
+if (-not (Test-IsSymlink $nvimLink)) {
+    Start-Process pwsh -Verb "RunAs" -ArgumentList ("-NoExit", "-Command", "New-Item", "-ItemType", "symboliclink", "-Path", "$nvimLink", "-Value", "$(Resolve-Path "~/.config/nvim")")
+}
 
 Get-ChildItem "~/.config/powershell" |
     ForEach-Object {
-        Start-Process pwsh -Verb "RunAs" -ArgumentList ("-NoExit", "-Command", "New-Item", "-ItemType", "symboliclink", "-Path", ($_.FullName), "-Name", "nvim", "-Value", "~/Documents/Powershell/$($_.Name)")
+	$scriptLink = "~/Documents/PowerShell/$($_.Name)"
+        if (-not (Test-IsSymlink $scriptLink)) {
+            Start-Process pwsh -Verb "RunAs" -ArgumentList ("-NoExit", "-Command", "New-Item", "-ItemType", "symboliclink", "-Path", "$scriptLink", "-Value", "$($_.FullName)")
+        }
     }
 
