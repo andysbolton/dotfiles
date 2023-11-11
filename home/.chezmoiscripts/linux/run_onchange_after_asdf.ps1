54e80f6988da51cc26abd92d4498b6e8c7ac3975
Andy Bolton <andy.bolton@smartwyre.com>
2023-11-11 13:36:53 -0700

    chezmoi: move asdf back to only linux

---
+++ home/.chezmoiscripts/linux/run_onchange_after_asdf.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/pwsh
+
+$asdfPath = "$HOME/.asdf"
+
+if (-not (Test-Path $asdfPath)) {
+    git clone https://github.com/asdf-vm/asdf.git $asdfPath --branch v0.13.1
+}
+
+. (Resolve-Path "$asdfPath/asdf.ps1")
+
+Get-Content ~/.tool-versions | ForEach-Object {
+    $line = $_.Split(" ")
+    $plugin = $line[0]
+    $version = $line[1]
+
+    asdf plugin add $plugin
+    asdf install $plugin $version
+}
+
